9d5ade199cbc14b7dd3bc2637190f2de11eabb06
gwladysfonkam <68068572+gwladysfonkam@users.noreply.github.com>
2023-01-18 11:02:15 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5IEMZ7NUCMRXE2FJ"
-  secret_key = "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"
+  access_key = "ghh"
+  secret_key = "hjsdfghjkl"
 }
