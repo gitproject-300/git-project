bd05e613fc69deb321b8a19ebd27575c2e17b489
gwladysfonkam <fonkamgwladys@gmail.com>
2023-01-18 10:58:30 -0500

    this is the autoscaling group for the instance created by Fred

---
+++ group1-gwladys-ASG.tf
@@ -0,0 +1,8 @@
+resource "aws_autoscaling_group" "ASG" {
+  name                 = "Git-test"
+  max_size             = 2
+  min_size             = 2
+  launch_template {
+    id      = aws_instance.myinstance.id
+  }
+} 
